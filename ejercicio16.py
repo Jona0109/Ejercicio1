ac306b29181dbcc0e179469cec30b1eb0b7b7e02
Jona0109 <131625141+Jona0109@users.noreply.github.com>
2023-05-07 17:22:55 -0500

    Ejercicio 1 Python

---
+++ ejercicio16.py
@@ -0,0 +1,5 @@
+import numpy as np  
+import pandas as pd 
+import folium       
+m = folium.Map()
+m.show_in_browser()
---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x=[1,2,3]
+y=[5,7,4]
+
+plt.plot(x,y)
+plt.xlabel('Eje X') 
+plt.ylabel('Eje Y')
+plt.title('Grafica lineal')
+plt.show()
